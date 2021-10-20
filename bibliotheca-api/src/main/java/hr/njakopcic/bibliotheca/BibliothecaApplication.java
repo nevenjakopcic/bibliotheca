b3d93b6fefbee45f2624738a960bc531ee0eb806
Neven Jakopčić <nevenjakopcic@gmail.com>
2021-10-20 20:28:52 +0200

    Initial commit

---
+++ bibliotheca-api/src/main/java/hr/njakopcic/bibliotheca/BibliothecaApplication.java
@@ -0,0 +1,13 @@
+package hr.njakopcic.bibliotheca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliothecaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliothecaApplication.class, args);
+    }
+
+}
